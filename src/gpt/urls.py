481349c740aeb9eed5fa70dce1d73f7a76bff81c
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-06-16 13:57:13 +0500

    add about page and footer

--- src/gpt/urls.py
+++ src/gpt/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("ask-gpt/", views.ask_gpt, name="ask_gpt"),
     path("new-chat/", views.new_chat, name="new_chat"),
     path("error-handler/", views.error_handler, name="error_handler"),
+    path("about/", views.about_handler, name="about"),
     path("replies/", views.ReplyListView.as_view(), name="reply_list"),
     path("reply/<uuid:pk>/", views.ReplyDetailView.as_view(), name="reply_detail"),
 ]
